9d422184ab76bf4a3e21b801a34f52ed294914c7
Christopher Dalziel <53733670+ChristopherDalziel@users.noreply.github.com>
2024-05-17 01:42:45 +0000

    re-render on update task, improve async calls

--- taskListFrontend/src/TaskList.js
+++ taskListFrontend/src/TaskList.js
@@ -6,38 +6,53 @@ const TaskList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
     const [error, setError] = useState();
-    const url = 'https://ideal-potato-rj76w6gp5q7hww57-5000.app.github.dev/'
+    const url = 'https://cautious-barnacle-w576gg5v9r5fgjrr-5000.app.github.dev/'
 
     useEffect(() => {
         fetchTasks();
     }, []);
 
-    const fetchTasks = () => {
+    const fetchTasks = async () => {
         setError(false);
-
-        axios
-            .get(`${url}/tasks`)
-            .then(response => {
+            try {
+                const response = await axios.get(`${url}/tasks`)
                 setTasks(response.data)
-            })
-            .catch((e) => {
+            } catch (error) {
                 setError(true)
-            });
+            }
 
     };
 
-const createTask = (title) => {
-    axios.post(`${url}task`, {title}).then(() => fetchTasks())
+const createTask = async (e, title) => {
+    e.preventDefault()
+
+    try {
+        await axios.post(`${url}task`, {title})
+        fetchTasks()
+    } catch (error) {
+        console.error(`Error creating task ${title}`)
+        setError(true)
+    } finally {
+        setNewTask('')
+    }
 }
 
-const updateTask = (task) => {
-    axios.put(`${url}task/${task.id}`, {done: !task.done})
+
+const updateTask = async (task) => {
+    try {
+        await axios.put(`${url}task/${task.id}`, {done: !task.done})    
+        setTasks(prevTasks => prevTasks.map(prevTask => prevTask.id === task.id ? {...prevTask, done: !prevTask.done} : prevTask))
+    } catch (error) {
+        console.error(`Error updating task ${task.id}: ${error}`)
+        setError(true)
+    }
+    
 }
 
 const deleteTask = async (id) => {
     try {
         await axios.delete(`${url}task/${id}`)
-        setTasks(prevTasks => prevTasks.filter(task => task.id !== id))
+        setTasks(prevTasks => prevTasks.filter(prevTask => prevTask.id !== id))
     } catch (error) {
         setError(true)
     }
@@ -45,16 +60,19 @@ const deleteTask = async (id) => {
 
     return (
         <div className="task-list-container">
-            <h1>Rocket 2.0</h1>
+            <h1>Rocket 2.0 🚀</h1>
            <div className="search-container"> 
-           <input
-                type="text"
-                className="task-input"
-                placeholder="Enter task"
-                value={newTask}
-                onChange={e => setNewTask(e.target.value)}
-            />
-            <button onClick={ ()=> createTask(newTask)}>Add New Task</button>
+           <form onSubmit={(e) => createTask(e, newTask)}> 
+                <input
+                    type="text"
+                    className="task-input"
+                    placeholder="Enter task"
+                    value={newTask}
+                    onChange={e => setNewTask(e.target.value)}
+                />
+                <button type="submit">Add New Task</button>
+           </form>
+
             </div>
             {error && <p className="notFound">Error, please try again!</p>}
             <ul>
@@ -65,9 +83,8 @@ const deleteTask = async (id) => {
                             checked={task.done}
                             onClick={ ()=> updateTask(task)}/>
                         <span style={{textDecoration: task.done ? 'line-through' : 'none'}}>
-                        {task.title}
-                    </span>
-                    <button onClick={ ()=> deleteTask(task.id)}>Delete Task</button>
+                        {task.title}</span>
+                        <button onClick={ ()=> deleteTask(task.id)}>Delete Task</button>
                     </li>
                 ))}
             </ul>
